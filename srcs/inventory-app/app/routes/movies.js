59e2f00c903fc5cec2aef5fb3f5b139d92ec08db
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2023-07-03 18:14:45 +0100

    remove the route for getMoviesByTitle and implement in the getMovies

--- srcs/inventory-app/app/routes/movies.js
+++ srcs/inventory-app/app/routes/movies.js
@@ -2,7 +2,6 @@ import movies from '../controllers/movies.js';
 export default (app, db) => {
     const controller = movies(db);
     app.get('/api/movies', controller.getMovies);
-    app.get('/api/movies?title=[name]', controller.getMoviesByTitle);
     app.post('/api/movies', controller.createMovie);
     app.delete('/api/movies', controller.deleteAllMovie);
     app.get('/api/movies/:id', controller.getMovieById);
