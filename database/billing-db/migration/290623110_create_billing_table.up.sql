8ef76aea950505ac72f16eefc297eee11e4781f2
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2023-07-05 13:09:13 +0100

    fix bug - remove the comma on line 5

--- database/billing-db/migration/290623110_create_billing_table.up.sql
+++ database/billing-db/migration/290623110_create_billing_table.up.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS "orders" (
     "id" UUID PRIMARY KEY,
     "user_id" UUID NOT NULL,
     "number_of_items" INT NOT NULL,
-    "total_price" INT NOT NULL,
+    "total_price" INT NOT NULL
 );
