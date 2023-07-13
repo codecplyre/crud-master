be4ceaffe68481c9a2e1fbf8607c0fea5f997873
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2023-07-13 11:52:06 +0100

    refactor - rearranged the routing

--- srcs/api-gateway/routes.js
+++ srcs/api-gateway/routes.js
@@ -1,20 +1,22 @@
 import { sendData } from './provider.js';
 import dotenv from 'dotenv';
+import express from 'express';
+
+const router = express();
 
 dotenv.config();
-export default (app) => {
-    app.post('/api/billing', async (req, res) => {
-        await sendData(req.body); // pass the data to the function we defined
-        console.log('A message is sent to queue', req.body);
-        res.send('Message Sent'); //response to the API request
-    });
-    app.get('/api/billing', async (req, res) => {
-        console.log('orders get request');
-        const response = await fetch(
-            `${process.env.BILLING_API_URL}/api/billing`
-        );
-        const data = await response.json();
-        res.send(data);
-        // res.send('orders get request');
-    });
-};
+
+router.post('/', async (req, res) => {
+    await sendData(req.body); // pass the data to the function we defined
+    console.log('A message is sent to queue', req.body);
+    res.send('Message Sent'); //response to the API request
+});
+router.get('/', async (req, res) => {
+    console.log('orders get request');
+    const response = await fetch(`${process.env.BILLING_API_URL}/api/billing`);
+    const data = await response.json();
+    res.send(data);
+    // res.send('orders get request');
+});
+
+export default router;
