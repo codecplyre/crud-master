51abe0b0f6d7e4972fecf08336054d719f7a8baf
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2023-07-13 15:08:27 +0100

    Documentation - add swagger documention
    add the initial documentation for the the billing api and inventory api
    
    this is just intial docs, it needs more improvement in terms of description

--- srcs/api-gateway/server.js
+++ srcs/api-gateway/server.js
@@ -2,16 +2,21 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { proxyMiddleware } from './proxy.js';
 import bodyParser from 'body-parser';
-import routes from './routes.js';
+import billingRoutes from './routes.js';
 import { connectQueue } from './provider.js';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 dotenv.config();
 const app = express();
 const port = process.env.API_GATEWAY_PORT || 3000;
 
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/movies', proxyMiddleware(process.env.MOVIES_API_URL));
 app.use(bodyParser.json());
-routes(app);
+app.use('/api/billing', billingRoutes);
 connectQueue(process.env.RABBITMQ_URL);
 app.listen(port, () => {
     console.log(`API Gateway is listening on port ${port}`);
